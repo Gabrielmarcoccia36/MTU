49eb6a99d127e1929f3d8b10cdfba5006ee1d772
Gabriel Marcoccia <gabrielmarcoccia36@gmail.com>
2022-01-26 20:16:32 -0500

    Added tiles and material of vibranium

---
+++ Items/Materials/Vibranium.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace MTU.Items.Materials
+{
+    class Vibranium : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Vibranium");
+            Tooltip.SetDefault("Strongest Metal of Earth-199999!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 8;
+            item.height = 8;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.useStyle = 1;
+            item.useTime = 10;
+            item.useAnimation = 10;
+            item.createTile = mod.TileType("VibraniumTile");
+            item.autoReuse = true;
+            item.material = true;
+        }
+    }
+}
---
+++ Items/Tiles/VibraniumTile.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ObjectData;
+
+namespace MTU.Items.Tiles
+{
+    class VibraniumTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+            Main.tileShine[Type] = 1000;
+
+
+            drop = mod.ItemType("ExampleTileItem");
+            dustType = DustID.Platinum;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Vibranium");
+
+            AddMapEntry(Color.AliceBlue);
+
+            minPick = 50;
+        }
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        public override bool Drop(int i, int j)
+        {
+            Tile t = Main.tile[i, j];
+            Item.NewItem(i * 16, j * 16, 16, 16, ModContent.ItemType<Items.Materials.Vibranium>());
+
+            return base.Drop(i, j);
+        }
+    }
+}
