629bde10bdae71089655febed8455195e72121f1
Lexnos <LexnosSingles@hotmail.com>
2022-01-26 19:29:04 -0500

    Scepter Added
    Basic Scepter implementation.

---
+++ Items/Weapons/Scepter.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MTU.Items.Weapons
+{
+    class Scepter : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sceptre");
+            Tooltip.SetDefault("Make the Mortals bow");
+            Item.staff[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 40;
+            item.magic = true;
+            item.mana = 5;
+            item.width = 35;
+            item.height = 35;
+            item.maxStack = 1;
+            item.value = Item.sellPrice(platinum: 10);
+            item.rare = ItemRarityID.Cyan;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.noMelee = false;
+            item.useTime = 40;
+            item.useAnimation = 40;
+            item.useTurn = true;
+            item.knockBack = 5;
+            item.autoReuse = true;
+            item.shootSpeed = 16f;
+            item.shoot = ProjectileID.ChargedBlasterOrb;
+        }
+
+    }
+}
