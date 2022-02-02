a5f83f577f66003ff7f6a73f06212171c6690df1
Gabriel Marcoccia <gabrielmarcoccia36@gmail.com>
2022-02-02 02:43:04 -0500

    Important script for hawkeye's bow and quiver

---
+++ Players/PlayerOne.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace MTU.Players
+{
+    public class PlayerOne : ModPlayer
+    {
+        public bool hasHawkQuiver;
+
+        public bool GetHasHawkQuiver()
+        {
+			return hasHawkQuiver;
+        }		
+	}
+}
