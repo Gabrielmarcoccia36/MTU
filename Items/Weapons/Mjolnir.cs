b1fa43154fb1cc9c102edb966aedac840b95121b
GabeHasWon <gabriel@guevarameyer.com>
2022-02-04 20:24:23 -0600

    AABBvLine lightning collision, cleanup, documentation, more lightning, better drawing

--- Items/Weapons/Mjolnir.cs
+++ Items/Weapons/Mjolnir.cs
@@ -9,10 +9,7 @@ namespace MTU.Items.Weapons
 {
     public class Mjolnir : ModItem
     {
-        public override void SetStaticDefaults()
-        {
-            Tooltip.SetDefault("Mjolnir");
-        }
+        public override void SetStaticDefaults() => Tooltip.SetDefault("'You are worthy!'");
 
         public override void SetDefaults()
         {
@@ -80,16 +77,16 @@ namespace MTU.Items.Weapons
 
         public override void AI()
         {
-            Player p = Main.player[projectile.owner];
+            Player p = Main.player[projectile.owner]; //Get player & set held projectile
             p.heldProj = projectile.whoAmI;
 
-            if (p.whoAmI != Main.myPlayer)
+            if (p.whoAmI != Main.myPlayer) //Only run this on the local player
                 return;
 
-            p.bodyFrame.Y = 56 * 2;
+            p.bodyFrame.Y = 56 * 2; //Set frame & position
             projectile.position = p.Center + new Vector2(0, p.gfxOffY);
 
-            if (!Main.mouseRight)
+            if (!Main.mouseRight) //Release when button is released
                 _hold = false;
 
             if (_hold)
@@ -99,18 +96,21 @@ namespace MTU.Items.Weapons
             }
             else
             {
-                if (_charge >= 50)
+                if (_charge >= 50) //If charge is high enough, spawn lightning
                 {
-                    Vector2 dir = projectile.DirectionTo(Main.MouseWorld);
-
-                    int proj = Projectile.NewProjectile(projectile.position + dir * 60, Vector2.Zero, ModContent.ProjectileType<MjolnirLightning>(), projectile.damage, 2f, projectile.owner);
-                    Projectile pr = Main.projectile[proj];
-                    pr.rotation = dir.ToRotation();
-
-                    if (pr.modProjectile is MjolnirLightning mjolnir)
+                    for (int i = 0; i < 3; ++i)
                     {
-                        mjolnir.constDir = dir;
-                        mjolnir.direction = dir;
+                        Vector2 dir = projectile.DirectionTo(Main.MouseWorld).RotatedByRandom(0.02f);
+
+                        int proj = Projectile.NewProjectile(projectile.position + dir * 60, Vector2.Zero, ModContent.ProjectileType<MjolnirLightning>(), projectile.damage, 2f, projectile.owner);
+                        Projectile pr = Main.projectile[proj];
+                        pr.rotation = dir.ToRotation();
+
+                        if (pr.modProjectile is MjolnirLightning mjolnir)
+                        {
+                            mjolnir.constDir = dir;
+                            mjolnir.direction = dir;
+                        }
                     }
                 }
 
@@ -120,19 +120,22 @@ namespace MTU.Items.Weapons
 
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
-            Texture2D tex = Main.projectileTexture[projectile.type];
+            Texture2D tex = Main.projectileTexture[projectile.type]; //Draw info
             Vector2 origin = new Vector2(0, tex.Height);
             Vector2 drawPos = projectile.position - Main.screenPosition;
             float rotation = -MathHelper.PiOver4;
 
-            spriteBatch.Draw(tex, drawPos, null, Color.White, rotation, origin, Vector2.One, SpriteEffects.None, 0f);
+            spriteBatch.Draw(tex, drawPos, null, Lighting.GetColor((int)projectile.position.X / 16, (int)projectile.position.Y / 16), rotation, origin, Vector2.One, SpriteEffects.None, 0f); //Draw base projectile
 
-            if (_charge > 50f)
+            if (_charge > 50f) //Draw empowered mjolnir (shows charge)
             {
                 Vector2 off = new Vector2(-tex.Width, tex.Height).RotatedBy(rotation) / 2f;
-                float sine = 1 + ((float)Math.Sin((_charge - 50f) * 0.02f) * 0.2f);
 
-                spriteBatch.Draw(tex, drawPos - off, null, Color.White * 0.5f, rotation, tex.Size() / 2f, Vector2.One * sine, SpriteEffects.None, 0f);
+                float Sine(float strength = 1f) => 1 + ((float)Math.Sin((_charge - 50f) * 0.02f) * 0.2f * strength);
+                Vector2 Shake() => new Vector2(Main.rand.NextFloat(-1f, 1f), Main.rand.NextFloat(-1f, 1f));
+
+                spriteBatch.Draw(tex, drawPos - off + Shake(), null, Color.White * 0.5f, rotation, tex.Size() / 2f, Vector2.One * Sine(), SpriteEffects.None, 0f);
+                spriteBatch.Draw(tex, drawPos - off + Shake(), null, Color.White * 0.25f, rotation, tex.Size() / 2f, Vector2.One * Sine(2f), SpriteEffects.None, 0f);
             }
             return false;
         }
@@ -144,26 +147,28 @@ namespace MTU.Items.Weapons
         public Vector2 direction = default;
         public int repeats = 0;
 
+        public const int MaxTimeLeft = 15;
+
         public override void SetDefaults()
         {
-            projectile.width = 40;
-            projectile.height = 40;
+            projectile.width = 30;
+            projectile.height = 30;
             projectile.friendly = true;
             projectile.penetrate = -1;
             projectile.tileCollide = false;
             projectile.ignoreWater = true;
             projectile.ranged = true;
             projectile.aiStyle = -1;
-            projectile.timeLeft = 15;
+            projectile.timeLeft = MaxTimeLeft;
         }
 
         public override void AI()
         {
             projectile.rotation = direction.ToRotation();
 
-            if (projectile.timeLeft == 14 && !Collision.SolidCollision(projectile.position, projectile.width, projectile.height))
+            if (projectile.timeLeft == MaxTimeLeft - 1 && !Collision.SolidCollision(projectile.position, projectile.width, projectile.height)) //Spawn next segment if not in wall
             {
-                int proj = Projectile.NewProjectile(projectile.Center + (direction * 60), Vector2.Zero, ModContent.ProjectileType<MjolnirLightning>(), projectile.damage, 2f, projectile.owner);
+                int proj = Projectile.NewProjectile(projectile.Center + (direction * 56), Vector2.Zero, ModContent.ProjectileType<MjolnirLightning>(), projectile.damage, 2f, projectile.owner);
                 Projectile pr = Main.projectile[proj];
 
                 Vector2 newDir = direction.RotatedByRandom(MathHelper.PiOver4);
@@ -179,10 +184,24 @@ namespace MTU.Items.Weapons
                 }
             }
 
-            if (Main.rand.NextFloat() <= 0.125f)
-                Dust.NewDustPerfect(projectile.Center, DustID.Electric, direction * Main.rand.NextFloat(5f, 10.5f));
+            if (Main.rand.NextFloat() <= 0.05f) //Dust
+                Dust.NewDustPerfect(projectile.Center, DustID.Electric, direction.RotatedByRandom(MathHelper.PiOver4) * Main.rand.NextFloat(7f, 14.5f));
         }
 
-        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) => target.AddBuff(BuffID.Electrified, 30);
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) => target.AddBuff(BuffID.Electrified, 30); //Inflict electrified on enemies
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return Collision.CheckAABBvLineCollision(targetHitbox.Location.ToVector2(), targetHitbox.Size(), projectile.position - direction * 30, projectile.position + direction * 30);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D tex = Main.projectileTexture[projectile.type]; //Draw info
+            Vector2 drawPos = projectile.Center - Main.screenPosition;
+
+            spriteBatch.Draw(tex, drawPos, null, Color.White, projectile.rotation, tex.Size() / 2f, Vector2.One, SpriteEffects.None, 0f); //Draw base item
+            return false;
+        }
     }
 }
