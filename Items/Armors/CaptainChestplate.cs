75ed424933d596cf4bcd80b3c5be2af901e3a49a
Lexnos <LexnosSingles@hotmail.com>
2022-02-04 21:37:34 -0500

    Captain America Added
    Captain America Helmet Implemented
    Captain America Chestplate Implemented
    Captain America Leggings implemented.

---
+++ Items/Armors/CaptainChestplate.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MTU.Items.Armors
+{
+    [AutoloadEquip(EquipType.Body)]
+    class CaptainChestplate : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Captain America Chestplate");
+            Tooltip.SetDefault("Feel the History");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 20;
+            item.maxStack = 1;
+            item.value = Item.sellPrice(silver: 20);
+            item.rare = ItemRarityID.Green;
+            item.defense = 4;
+        }
+
+
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar, 30);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Armors/CaptainHelmet.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using MTU.Items.Buffs;
+
+namespace MTU.Items.Armors
+{
+    [AutoloadEquip(EquipType.Head)]
+    class CaptainHelmet : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Captain America Helmet");
+            Tooltip.SetDefault("Feel the History");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 18;
+            item.maxStack = 1;
+            item.value = Item.sellPrice(silver: 10);
+            item.rare = ItemRarityID.Green;
+            item.defense = 2;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ModContent.ItemType<CaptainChestplate>() && legs.type == ModContent.ItemType<CaptainLeggings>();
+        }
+
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = "Melee Damage increased";
+            //player.AddBuff(mod.BuffType("MarkIBuff"), 2);
+            base.UpdateArmorSet(player);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar, 20);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
