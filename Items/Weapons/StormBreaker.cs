2a0b2ac389098514f6615bd8e982842bd1272243
Lexnos <LexnosSingles@hotmail.com>
2022-02-05 02:19:00 -0500

    Storm Breaker Slash Implemented
    Sprite for the Storm Breaker Slash has been added
    Script for Storm Breaker updated.

--- Items/Weapons/StormBreaker.cs
+++ Items/Weapons/StormBreaker.cs
@@ -1,6 +1,8 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
 
 namespace MTU.Items.Weapons
 {
@@ -14,20 +16,47 @@ namespace MTU.Items.Weapons
 
         public override void SetDefaults()
         {
-            item.damage = 120;
-            item.width = 40;
-            item.height = 40;
+            item.damage = 190;
+            item.width = 54;
+            item.height = 54;
             item.maxStack = 1;
             item.value = Item.sellPrice(platinum: 10);
             item.rare = ItemRarityID.Cyan;
             item.useStyle = ItemUseStyleID.SwingThrow;
-            item.useTime = 20;
-            item.useAnimation = 20;
+            item.useTime = 30;
+            item.useAnimation = 30;
             item.useTurn = true;
             item.knockBack = 5;
             item.autoReuse = true;
             item.melee = true;
+            item.shoot = ModContent.ProjectileType<StormBreakerSlash>();
+            item.shootSpeed = 12;
+
         }
 
+    }
+
+    public class StormBreakerSlash : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.Size = new Vector2(40);
+            projectile.aiStyle = 1;
+
+            projectile.friendly = true;
+
+            projectile.penetrate = -1;
+
+            projectile.ignoreWater = true;
+            projectile.tileCollide = true;
+
+            aiType = ProjectileID.FrostWave;
+        }
     }
 }
